f43e6e61143106aec8dc95b8fc983b38653b65c6
Sahar Husseini <husseinisahar7@gmail.com>
2020-06-16 13:20:58 +0200

    comments added to main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import Model.Edge;
 import Model.Stop;
 import Unweighted.BFSShortestPath;
@@ -10,11 +9,10 @@ import Unweighted.UnweightedGraph;
 
 public class Main {
 	static ReadCSV rc;
-
 	/**
 	 *reads "stop_times.csv" file, which contains Times that a vehicle arrives at and departs from
 	 * individual stops for each trip. If stop_sequence is equal to 1 it means that new trip started.
-	 * @return
+	 * @return an array of array. Each inner array contains stop ids belonging to one completed trip.
 	 */
 	public static ArrayList<ArrayList<Integer>> getTripList() {
 		// Read stop_times.csv, find each complete trip
@@ -40,7 +38,8 @@ public class Main {
 
 	/**
 	 *
-	 * @param list of the existing trips
+	 * @param list of the existing trips. Input is array of array with stop ids.
+	 * We create a new list and add the trip list to the new list if it does not already exist.
 	 * @return  return list of the trips without duplicates
 	 */
 	public static ArrayList<ArrayList<Integer>> removeDuplicates(ArrayList<ArrayList<Integer>> list) {
@@ -53,6 +52,14 @@ public class Main {
 		return newList;
 	}
 
+	/**
+	 * In this method we read the list of the stops and create objects for each stop
+	 * the stop_id, stop_name, stop_lat and stop_lon are given as a parameter to the
+	 * Stop object.
+	 * @param stops array of all stops.
+	 * @return the list of all Stop pbjects. each object has stop_id, stop_name,
+	 * stop_lat and stop_lon features.
+	 */
 	public static ArrayList<Stop> getStopsList(ArrayList<String[]> stops) {
 		ArrayList<Stop> stops_list = new ArrayList<Stop>();
 		for (int i = 0; i < stops.size(); i++) {
@@ -66,6 +73,12 @@ public class Main {
 		return stops_list;
 	}
 
+	/**
+	 * checks for the ids of the stops and return them
+	 * @param stopId stop id
+	 * @param stops list of stops
+	 * @return reurns object corresponding to stop id
+	 */
 	public static Stop getStopById(int stopId, ArrayList<Stop> stops) {
 		for (Stop s : stops) {
 			int stop_id = s.stop_id;
@@ -103,48 +116,53 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		
+
 		// Get trips list by stop_times.csv
 		ArrayList<ArrayList<Integer>> trips_list = getTripList();
-		
+
 		// Build unweighed graph
 		UnweightedGraph uwGraph = new UnweightedGraph(trips_list);
 
 		// Build weighted graph
 		ArrayList<Edge> weighed_edge_list = new ArrayList<Edge>();
+
 		// Read Stop.csv for getting position
 		ArrayList<String[]> stops = rc.readCSV("stops.csv");
 		ArrayList<Stop> stops_list = getStopsList(stops);
+
 		// Get all edges, build a Map(Edge, distance)
 		for (int stop1_id : uwGraph.uw_map.keySet()) {
-			ArrayList<Integer> neigbors_list = uwGraph.uw_map.get(stop1_id);
+			ArrayList<Integer> neigbors_list = uwGraph.uw_map.get(stop1_id); //take neighboring nodes
+			//this for loop is for finding all the distances between the vertex and its neighboring vertices.
 			for (int stop2_id : neigbors_list) {
-				Stop stop1 = getStopById(stop1_id, stops_list);
-				Stop stop2 = getStopById(stop2_id, stops_list);
+				Stop stop1 = getStopById(stop1_id, stops_list); // stop 1
+				Stop stop2 = getStopById(stop2_id, stops_list);	//neighbor stop
+				//computing distance between two vertices
 				double distance = calculDistanceByGEO(stop1.getStop_lat(), stop1.getStop_lon(), stop2.getStop_lat(),
 						stop2.getStop_lon());
+				//creating adge between two vertices
 				Edge edge = new Edge(stop1_id, stop2_id, distance);
 				weighed_edge_list.add(edge);
 			}
 		}
 
-		// print
 		for (Edge edge : weighed_edge_list) {
-//			System.out.println(edge.stop1_id + " - " + edge.stop2_id + ": distance " + edge.distance);
+ 			System.out.println(edge.stop1_id + " - " + edge.stop2_id + ": distance " + edge.distance);
 		}
 
 		// BFS shortest path in unweighted Graph
 		int test_start_stop_id = 7310;
 		int test_target_stop_id = 1974;
-//		ArrayList<Integer> bfsVisitedPath = BFSShortestPath.bfs( test_start_stop_id, test_target_stop_id,uwGraph);
-//		
+		//ArrayList<Integer> bfsVisitedPath = BFSShortestPath.bfs( test_start_stop_id, test_target_stop_id,uwGraph);
+
 //		System.out.println("=========== (BFS shortest path) 7310 to 1974 ===========");
 //		for(int i:bfsVisitedPath) {
 //			System.out.print(i+ " ");
 //		}
 //		System.out.println();
 		
-		// Build Weighted Graph
-		// ...
+		 //Build Weighted Graph
+		 //...
+
 	}
 }
--- src/Unweighted/UnweightedGraph.java
+++ src/Unweighted/UnweightedGraph.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class represents a unweighted graph. The stops are the vertices of the
+ * graph and if there is a trip between stops we create an edge between them. here
+ * edges are equal to 0. because we have unweighted graph. The stops are the key of
+ * the map and neighboring stops are the value of the map.
+ */
 public class UnweightedGraph {
 
 	public int curr_stop_id = 0;
+	// hashmap, which contains stop id as key and connected stops with this station are
+	//the value of the map.
 	public Map<Integer, ArrayList<Integer>> uw_map = new HashMap<Integer, ArrayList<Integer>>();
 
+	/**
+	 *
+	 * @param trips_list takes as input trip list which contains arrays of stop ids.
+	 */
 	public UnweightedGraph(ArrayList<ArrayList<Integer>> trips_list) {
 		// Map (stop_id, {stop1_id, stop2_id})
 		int first_stop_id = 0;
